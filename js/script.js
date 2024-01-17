fe83b3193a94c027297ca8b353a2deef87a23542
redihoxhaa <redi.hoxha1996@gmail.com>
2024-01-17 01:39:56 +0100

    feat: fix parametri funzione get Products e condizione isset -> !empty server

--- js/script.js
+++ js/script.js
@@ -11,23 +11,16 @@ createApp({
     methods: {
 
         getProducts() {
-            const params = {};
-
-            if (this.selectedCategory !== "") {
-                params.category = this.selectedCategory;
-            }
-
-            if (this.selectedType !== "") {
-                params.type = this.selectedType;
-            }
-
             axios.get("server/server.php", {
-                params: params
+                params: {
+                    "category": this.selectedCategory,
+                    "type": this.selectedType,
+                }
             }).then(response => {
                 if (response.status === 200) {
                     this.products = response.data;
                 }
-            });
+            })
         }
     },
     mounted() {
--- server/server.php
+++ server/server.php
@@ -4,14 +4,17 @@ require_once __DIR__ . '/../database/db.php';
 
 $result = $products_db;
 
-if (isset($_GET['category'])) {
+// Con USE permettiamo alla funzione di leggere la variabile che passiamo tra parentesi,
+// in questo caso viene usata come condizione per inserire il prodotto nell'array (true).
+
+if (!empty($_GET['category'])) {
     $selectedCategory = $_GET['category'];
     $result = array_filter($result, function ($product) use ($selectedCategory) {
         return $product['category'] === $selectedCategory;
     });
 }
 
-if (isset($_GET['type'])) {
+if (!empty($_GET['type'])) {
     $selectedType = $_GET['type'];
     $result = array_filter($result, function ($product) use ($selectedType) {
         return $product['type'] === $selectedType;
